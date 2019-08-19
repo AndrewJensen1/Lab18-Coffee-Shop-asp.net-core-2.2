3fd7be9deb5f9a665f8f3340b6719ae3fe8299d7
Andrew <Jensen1.Andrew@gmail.com>
2019-08-19 08:31:52 -0400

    first commit

---
+++ Lab18 Coffee Shop/Controllers/CoffeeHouseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab18_Coffee_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18_Coffee_Shop.Controllers
+{
+    public class CoffeeHouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View(new RegisterUser());
+        }
+        [HttpPost]
+        public IActionResult Registration(RegisterUser newUser)
+        {
+            return RedirectToAction("Welcome", newUser);
+        }
+
+        public IActionResult Welcome(RegisterUser user)
+        {
+            return View(user);
+        }
+
+
+    }
+}
